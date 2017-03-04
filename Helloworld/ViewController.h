1c4c13d3c3df9e1ca990018094ea9a3f32924e8f
user <user@mini-4.dlink>
2017-03-04 08:56:17 +0400

    Initial Commit

---
+++ Helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Helloworld
+//
+//  Created by user on 04.03.17.
+//  Copyright © 2017 edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Helloworld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Helloworld
+//
+//  Created by user on 04.03.17.
+//  Copyright © 2017 edu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
